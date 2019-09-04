428d5b1fb422f46535f7debebc15a1e93e029864
Shubhambh18 <mindfak3@outlook.com>
2019-09-03 23:51:16 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import { Route, Link} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import Register from './Register/Register.react';
+import LoginPage from './LoginPage/LoginPage.react';
 ReactDOM.render(
   <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>
   ,
