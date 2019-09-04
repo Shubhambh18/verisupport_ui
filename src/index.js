530df3fe7c1e303a0bb72b466c7f65c87dc2ff74
Shubhambh18 <mindfak3@outlook.com>
2019-09-03 23:51:00 -0700

    Added Browser Router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>
+  <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>
   ,
   document.getElementById('root')
 );
